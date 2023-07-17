1cdc9e8a7d956386657f646c5bec3518e70fb62d
Douglas Araujo <douglasaraujosampaio@gmail.com>
2023-07-17 11:44:58 -0300

    first commit

---
+++ src/main/java/br/com/douglas/travelbooking/TravelbookingApplication.java
@@ -0,0 +1,13 @@
+package br.com.douglas.travelbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelbookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelbookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/douglas/travelbooking/TravelbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.douglas.travelbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelbookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
