785b02d8af6566ab86ab56e4636c021fbfd86309
Douglas Araujo <douglasaraujosampaio@gmail.com>
2023-07-19 12:11:38 -0300

    feat: criado nova rota de update e delete de clinica, e passado a responsabilidade do controller para o service, de tal maneira que o controller fique responsavel so pela chamada do service

--- src/main/java/br/com/douglas/travelbooking/controllers/ClinicaController.java
+++ src/main/java/br/com/douglas/travelbooking/controllers/ClinicaController.java
@@ -1,23 +1,22 @@
 package br.com.douglas.travelbooking.controllers;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.douglas.travelbooking.dto.ClinicaCreateDTO;
 import br.com.douglas.travelbooking.dto.ClinicaDTO;
-import br.com.douglas.travelbooking.dto.EnderecoDTO;
 import br.com.douglas.travelbooking.model.Clinica;
-import br.com.douglas.travelbooking.model.Endereco;
 import br.com.douglas.travelbooking.services.ClinicaService;
 
 @RestController
@@ -29,25 +28,7 @@ public class ClinicaController {
 
 	@GetMapping
 	public ResponseEntity<List<ClinicaDTO>> findAll() {
-		List<Clinica> list = service.findAll();
-		List<ClinicaDTO> listDto = list.stream().map(listClinica -> {
-			ClinicaDTO clinicaDto = new ClinicaDTO();
-			clinicaDto.setIdClinica(listClinica.getIdClinica());
-			clinicaDto.setNomeClinica(listClinica.getNomeClinica());
-
-			EnderecoDTO enderecoDto = new EnderecoDTO();
-			enderecoDto.setNumero(listClinica.getEndereco().getNumero());
-			enderecoDto.setCep(listClinica.getEndereco().getCep());
-			enderecoDto.setLogradouro(listClinica.getEndereco().getLogradouro());
-			enderecoDto.setComplemento(listClinica.getEndereco().getComplemento());
-			enderecoDto.setBairro(listClinica.getEndereco().getBairro());
-			enderecoDto.setUf(listClinica.getEndereco().getUf());
-
-			clinicaDto.setEndereco(enderecoDto);
-
-			return clinicaDto;
-		}).collect(Collectors.toList());
-
+		List<ClinicaDTO> listDto = service.findAll();
 		return ResponseEntity.ok().body(listDto);
 	}
 
@@ -64,21 +45,25 @@ public class ClinicaController {
 
 	@PostMapping
 	public ResponseEntity<ClinicaDTO> insert(@RequestBody ClinicaCreateDTO objClinicaDto) {
-
-		Endereco apiCep = service.consultarCep(objClinicaDto.getCep());
-		if (apiCep == null) {
-			throw new RuntimeException("Não foi possível obter informações do CEP: " + objClinicaDto.getCep());
-		}
-		int id = service.getClinicaCount();
-		ClinicaDTO response = new ClinicaDTO(id, objClinicaDto.getNomeClinica(),
-				new EnderecoDTO(objClinicaDto.getNumero(), objClinicaDto.getCep(), apiCep.getLogradouro(),
-						apiCep.getComplemento(), apiCep.getBairro(), apiCep.getUf()));
-
-		Clinica objClinica = service.clinicaCreateDTO(objClinicaDto);
-		objClinica.setIdClinica(id);
-		objClinica = service.insert(objClinica);
-
+		ClinicaDTO response = service.insert(objClinicaDto);
 		return ResponseEntity.status(HttpStatus.CREATED).body(response);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<ClinicaDTO> update(@PathVariable int id, @RequestBody ClinicaCreateDTO objClinicaDto) {
+		ClinicaDTO updatedClinica = service.update(id, objClinicaDto);
+		return ResponseEntity.ok(updatedClinica);
+	}
+
+	@DeleteMapping("/{idClinica}")
+	public ResponseEntity<Void> deleteClinica(@PathVariable int idClinica) {
+		Clinica clinica = service.findByClinicaId(idClinica);
+		if (clinica != null) {
+			service.deleteClinica(idClinica);
+			return ResponseEntity.noContent().build();
+		} else {
+			return ResponseEntity.notFound().build();
+		}
 	}
 
 }
--- src/main/java/br/com/douglas/travelbooking/services/ClinicaService.java
+++ src/main/java/br/com/douglas/travelbooking/services/ClinicaService.java
@@ -1,6 +1,7 @@
 package br.com.douglas.travelbooking.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,83 +11,127 @@ import org.springframework.web.client.RestTemplate;
 
 import br.com.douglas.travelbooking.dto.ClinicaCreateDTO;
 import br.com.douglas.travelbooking.dto.ClinicaDTO;
+import br.com.douglas.travelbooking.dto.EnderecoDTO;
 import br.com.douglas.travelbooking.model.Clinica;
 import br.com.douglas.travelbooking.model.Endereco;
 import br.com.douglas.travelbooking.repository.ClinicaRepository;
 
 @Service
 public class ClinicaService {
-	
+
 	@Autowired
 	private ClinicaRepository repository;
-	
+
 	@Autowired
-    private RestTemplate restTemplate;
-	
+	private RestTemplate restTemplate;
+
 	@Value("${api.cep.url}")
-    private String cepApiUrl;
-	
-	public  List<Clinica> findAll(){
-		return repository.findAll();
-	}
-	
-	public Clinica insert(Clinica obj) {
-		return repository.insert(obj);
-	}
-	
-	public Clinica findByClinicaId(int idClinica) {
-	    return repository.findByClinicaId(idClinica);
-	}
-	
-	public int getClinicaCount() {		
-	    Clinica lastClinica = repository.findTopByOrderByIdClinicaDesc();
-	    return (lastClinica != null) ? lastClinica.getIdClinica() + 1 : 1;
+	private String cepApiUrl;
+
+	public List<ClinicaDTO> findAll() {
+		List<Clinica> list = repository.findAll();
+		return list.stream().map(listClinica -> {
+			ClinicaDTO clinicaDto = new ClinicaDTO();
+			clinicaDto.setIdClinica(listClinica.getIdClinica());
+			clinicaDto.setNomeClinica(listClinica.getNomeClinica());
+
+			EnderecoDTO enderecoDto = new EnderecoDTO();
+			enderecoDto.setNumero(listClinica.getEndereco().getNumero());
+			enderecoDto.setCep(listClinica.getEndereco().getCep());
+			enderecoDto.setLogradouro(listClinica.getEndereco().getLogradouro());
+			enderecoDto.setComplemento(listClinica.getEndereco().getComplemento());
+			enderecoDto.setBairro(listClinica.getEndereco().getBairro());
+			enderecoDto.setUf(listClinica.getEndereco().getUf());
+
+			clinicaDto.setEndereco(enderecoDto);
+
+			return clinicaDto;
+		}).collect(Collectors.toList());
 	}
 
-	
+	public ClinicaDTO insert(ClinicaCreateDTO clinicaDto) {
+		Endereco apiCep = consultarCep(clinicaDto.getCep());
+
+		if (apiCep == null) {
+			throw new RuntimeException("Não foi possível obter informações do CEP: " + clinicaDto.getCep());
+		}
+
+		int id = getClinicaCount();
+		ClinicaDTO response = new ClinicaDTO(id, clinicaDto.getNomeClinica(),
+				new EnderecoDTO(clinicaDto.getNumero(), clinicaDto.getCep(), apiCep.getLogradouro(),
+						apiCep.getComplemento(), apiCep.getBairro(), apiCep.getUf()));
+
+		Clinica objClinica = clinicaCreateDTO(clinicaDto);
+		objClinica.setIdClinica(id);
+		objClinica = repository.insert(objClinica);
+
+		return response;
+	}
+
+	public Clinica findByClinicaId(int idClinica) {
+		return repository.findByClinicaId(idClinica);
+	}
+
+	public ClinicaDTO update(int id, ClinicaCreateDTO clinicaDto) {
+		Clinica existingClinica = repository.findByClinicaId(id);
+
+		if (existingClinica == null) {
+			throw new RuntimeException("Clínica com ID " + id + " não encontrada.");
+		}
+
+		Endereco apiCep = consultarCep(clinicaDto.getCep());
+
+		if (apiCep == null) {
+			throw new RuntimeException("Não foi possível obter informações do CEP: " + clinicaDto.getCep());
+		}
+
+		existingClinica.setNomeClinica(clinicaDto.getNomeClinica());
+		existingClinica.getEndereco().setNumero(clinicaDto.getNumero());
+		existingClinica.getEndereco().setCep(clinicaDto.getCep());
+		existingClinica.getEndereco().setLogradouro(apiCep.getLogradouro());
+		existingClinica.getEndereco().setComplemento(apiCep.getComplemento());
+		existingClinica.getEndereco().setBairro(apiCep.getBairro());
+		existingClinica.getEndereco().setUf(apiCep.getUf());
+
+		existingClinica = repository.save(existingClinica);
+
+		return new ClinicaDTO(existingClinica);
+	}
+
+	public void deleteClinica(int idClinica) {
+		repository.deleteById(idClinica);
+	}
+
+	public int getClinicaCount() {
+		Clinica lastClinica = repository.findTopByOrderByIdClinicaDesc();
+		return (lastClinica != null) ? lastClinica.getIdClinica() + 1 : 1;
+	}
+
 	public Clinica clinicaDTO(ClinicaDTO clinicaDto) {
 		Endereco apiCep = consultarCep(clinicaDto.getEndereco().getCep());
-	    return new Clinica(
-	            clinicaDto.getIdClinica(),
-	            clinicaDto.getNomeClinica(),
-	            new Endereco(
-	                clinicaDto.getEndereco().getNumero(),
-	                clinicaDto.getEndereco().getCep(),
-	                apiCep.getLogradouro(),
-	                apiCep.getComplemento(),
-	                apiCep.getBairro(),
-	                apiCep.getUf()
-	            )
-	    );
+		return new Clinica(clinicaDto.getIdClinica(), clinicaDto.getNomeClinica(),
+				new Endereco(clinicaDto.getEndereco().getNumero(), clinicaDto.getEndereco().getCep(),
+						apiCep.getLogradouro(), apiCep.getComplemento(), apiCep.getBairro(), apiCep.getUf()));
 	}
-	
+
 	public Clinica clinicaCreateDTO(ClinicaCreateDTO clinicaDto) {
 		Endereco apiCep = consultarCep(clinicaDto.getCep());
-	    return new Clinica(
-	            clinicaDto.getIdClinica(),
-	            clinicaDto.getNomeClinica(),
-	            new Endereco(
-	                clinicaDto.getNumero(),
-	                clinicaDto.getCep(),
-	                apiCep.getLogradouro(),
-	                apiCep.getComplemento(),
-	                apiCep.getBairro(),
-	                apiCep.getUf()
-	            )
-	    );
+		return new Clinica(clinicaDto.getIdClinica(), clinicaDto.getNomeClinica(),
+				new Endereco(clinicaDto.getNumero(), clinicaDto.getCep(), apiCep.getLogradouro(),
+						apiCep.getComplemento(), apiCep.getBairro(), apiCep.getUf()));
 	}
-	
-    public Endereco consultarCep(String cep) {
 
-        String url = cepApiUrl + cep + "/json";
-        
-        ResponseEntity<Endereco> response = restTemplate.getForEntity(url, Endereco.class);
-        
-        if (response.getStatusCode().is2xxSuccessful()) {
-            return response.getBody();
-        } else {
-            throw new RuntimeException("Não foi possível consultar o CEP: " + cep);
-        }
-    }
+	public Endereco consultarCep(String cep) {
+
+		String url = cepApiUrl + cep + "/json";
+
+		ResponseEntity<Endereco> response = restTemplate.getForEntity(url, Endereco.class);
+
+		if (response.getStatusCode().is2xxSuccessful()) {
+			return response.getBody();
+		} else {
+			throw new RuntimeException("Não foi possível consultar o CEP: " + cep);
+		}
+	}
 
 }
